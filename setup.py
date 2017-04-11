427387c461bcb1534bf7d6e9b9ff3e105685eaaf
Pomax <pomax@nihongoresources.com>
2017-04-11 13:51:53 -0700

    0.0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'filebrowser-s3',
   packages = ['filebrowser-s3'],
-  version = '0.0.3',
+  version = '0.0.4',
   description = 'an S3 fix for Mezzanine\'s media manager',
   author = 'Mozilla Foundation',
   author_email = 'pomax@mozillafoundation.org',
   url = 'https://github.com/Pomax/filebrowser-s3',
-  download_url = 'https://github.com/Pomax/filebrowser-s3/archive/0.0.3.tar.gz',
+  download_url = 'https://github.com/Pomax/filebrowser-s3/archive/0.0.4.tar.gz',
   keywords = ['mezzanine', 'filebrowser', 'filebrowser_safe', 'aws', 's3'],
   classifiers = [],
 )
