6aee660189dec6174b5a1249eb8fe14cfd2546fc
Pomax <pomax@nihongoresources.com>
2017-04-18 13:13:22 -0700

    find package

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
-  name = 'filebrowser_s3',
-  packages = ['filebrowser_s3'],
-  version = '0.1.14',
-  description = 'an S3 fix for Mezzanine\'s media manager',
-  author = 'Mozilla Foundation',
-  author_email = 'pomax@mozillafoundation.org',
-  url = 'https://github.com/Pomax/filebrowser_s3',
-  download_url = 'https://github.com/Pomax/filebrowser_s3/archive/0.1.14.tar.gz',
-  keywords = ['mezzanine', 'filebrowser', 'filebrowser_safe', 'aws', 's3'],
-  classifiers = [],
-  include_package_data = True,
+	name="filebrowser_s3",
+	version="0.1.15",
+	description="An S3 fix for Mezzanine's media manager.",
+	author = 'Mozilla Foundation',
+	author_email = 'pomax@mozillafoundation.org',
+	maintainer="Pomax",
+	maintainer_email="pomax@mozillafoundation.org",
+	url = 'https://github.com/Pomax/filebrowser_s3',
+	download_url = 'https://github.com/Pomax/filebrowser_s3/archive/0.1.15.tar.gz',
+	packages=find_packages(),
+	include_package_data=True,
+	zip_safe=False,
+	classifiers=[],
 )
